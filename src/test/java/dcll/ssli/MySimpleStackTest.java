6b8dcc261b81d2e2754c2b66a8ff045d87bb96d5
Aleksandair <aleksandair.mh@gmail.com>
2015-03-04 11:26:26 +0100

    JUnit tests updated to JUnit4

--- src/test/java/dcll/ssli/MySimpleStackTest.java
+++ src/test/java/dcll/ssli/MySimpleStackTest.java
@@ -1,49 +1,65 @@
 package dcll.ssli;
 
 import junit.framework.TestCase;
+import org.junit.*;
 
-public class MySimpleStackTest extends TestCase {
+public class MySimpleStackTest {
 
+    SimpleStack stack;
+
+    @Before
+    public void setUp() throws Exception {
+        stack = new MySimpleStack();
+        System.out.println("Je suis exécuté avant chaque test.");
+    }
+
+    @Test
     public void testIsEmpty() throws Exception {
-        SimpleStack stack = new MySimpleStack();
-        assertTrue(stack.isEmpty());
+        Assert.assertTrue(stack.isEmpty());
 
-        stack.push(new Item(new Object()));
-        assertFalse(stack.isEmpty());
+        Item item = new Item(new String("toto"));
+        stack.push(item);
+        Assert.assertFalse(stack.isEmpty());
     }
 
+    @Test
     public void testGetSize() throws Exception {
-        SimpleStack stack = new MySimpleStack();
+        Assert.assertEquals(stack.getSize(), 0);
 
-        assertEquals(stack.getSize(), 0);
+        Item item = new Item(new Integer(5));
+        Item item1 = new Item(new Integer(8));
+        stack.push(item);
+        stack.push(item1);
+        Assert.assertEquals(stack.getSize(), 2);
+
+        Item item2 = stack.peek();
+        Assert.assertTrue(item2.getValue() instanceof Integer);
     }
 
+    @Test
     public void testPush() throws Exception {
-        SimpleStack stack = new MySimpleStack();
-        Item item = new Item(new Object());
-        stack.push(item);
+        Item item = new Item(new String("toto"));
+
     }
 
+    @Test
     public void testPeek() throws Exception {
-        SimpleStack stack = new MySimpleStack();
-        Item item = new Item(new Object());
+        Item item = new Item(new Integer(8));
         stack.push(item);
-
-        assertEquals(stack.peek(), item);
+        Assert.assertEquals(stack.getSize(), 1);
+        Assert.assertEquals(stack.peek(), item);
     }
 
+    @Test
     public void testPop() throws Exception {
-        SimpleStack stack = new MySimpleStack();
-        Item item = new Item(new Object());
+        Item item = new Item(new Integer(8));
+        Assert.assertEquals(stack.getSize(), 0);
         stack.push(item);
-        stack.push(item);
-
-        assertEquals(stack.pop(), item);
-        assertEquals(stack.getSize(), 1);
-        assertFalse(stack.isEmpty());
-
-        assertEquals(stack.pop(), item);
-        assertEquals(stack.getSize(), 0);
-        assertTrue(stack.isEmpty());
+        Assert.assertEquals(stack.getSize(), 1);
+        Item o = stack.pop();
+        Assert.assertEquals(stack.getSize(), 0);
+        Assert.assertTrue(o.getValue() instanceof Integer);
+        Integer integer = (Integer)o.getValue();
+        Assert.assertEquals(item.getValue(), integer);
     }
 }
