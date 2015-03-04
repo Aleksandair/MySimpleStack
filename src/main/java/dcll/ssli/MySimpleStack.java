cc8ec9c87b2c9f44219c011a74f038c8c89c94a8
Aleksandair <aleksandair.mh@gmail.com>
2015-03-04 10:18:37 +0100

    MySimpleStack implemented

--- src/main/java/dcll/ssli/MySimpleStack.java
+++ src/main/java/dcll/ssli/MySimpleStack.java
@@ -1,33 +1,38 @@
 package dcll.ssli;
 
 import java.util.EmptyStackException;
+import java.util.Stack;
 
 /**
+ * My implementation of SimpleStack
  * Created by Skander on 27/02/2015.
  */
 public class MySimpleStack implements SimpleStack {
+
+    private Stack<Item> items = new Stack<Item>();
+
     @Override
     public boolean isEmpty() {
-        return false;
+        return items.isEmpty();
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return items.size();
     }
 
     @Override
     public void push(Item item) {
-
+        items.push(item);
     }
 
     @Override
     public Item peek() throws EmptyStackException {
-        return null;
+        return items.peek();
     }
 
     @Override
     public Item pop() throws EmptyStackException {
-        return null;
+        return items.pop();
     }
 }
